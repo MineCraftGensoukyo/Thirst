de8ecb2991158a19168b71291f3708f443e98ad4
kaatenn <1924198467@qq.com>
2024-10-31 17:23:48 +0800

    添加Screen素材

---
+++ src/main/java/moe/gensoukyo/thirst/gui/screen/CisternScreen.java
@@ -0,0 +1,10 @@
+package moe.gensoukyo.thirst.gui.screen;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class CisternScreen extends Screen {
+    protected CisternScreen(Component pTitle) {
+        super(pTitle);
+    }
+}
